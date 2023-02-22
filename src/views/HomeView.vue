34adf03d47916d06ec5ec7c7569c926e74dd4ddd
karam mustafa <karam2mustafa@gmail.com>
2023-02-22 16:41:14 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Result is: {{ result }} </h1>
+
+    <label>First Number</label>
+    <input v-model="firstNumber" />
+    <label>Second Number</label>
+    <input v-model="secondNumber" />
+    <label>Operator</label>
+
+    <select v-model="operator">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="/">/</option>
+      <option value="*">*</option>
+    </select>
+
+    <button @click="calc">Calc</button>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+
+// MVVM
+// Model 
+// View
+// Binding
+
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      firstNumber: '',
+      secondNumber: '',
+      operator: '',
+      result: ''
+    }
+  },
+  methods: {
+    calc() {
+      this.result = eval(this.firstNumber + this.operator + this.secondNumber)
+    }
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<style>
+input,
+select {
+  margin: 30px 20px 0 0px;
+  display: block;
+}
+</style>
